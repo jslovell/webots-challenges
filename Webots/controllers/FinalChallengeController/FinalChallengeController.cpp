ac31a24e6fee3be43d87c6a4a7e0687d958e2bd3
Josh <jlovell@mail.bradley.edu>
2024-11-14 12:58:34 -0600

    Final challenge color setup

--- Webots/controllers/FinalChallengeController/FinalChallengeController.cpp
+++ Webots/controllers/FinalChallengeController/FinalChallengeController.cpp
@@ -7,6 +7,7 @@
 
 #define TIME_STEP 64
 #define MAX_SPEED .5
+#define MAX_TURN_SPEED .2
 #define MAX_CAMERA_SPEED 2.0
 #define MOVE_TIME 85
 
@@ -33,13 +34,13 @@ static void MoveBack() {
 }
 
 static void TurnLeft() {
-    leftMotor->setVelocity(-MAX_SPEED);
-    rightMotor->setVelocity(MAX_SPEED);
+    leftMotor->setVelocity(-MAX_TURN_SPEED);
+    rightMotor->setVelocity(MAX_TURN_SPEED);
 }
 
 static void TurnRight() {
-    leftMotor->setVelocity(MAX_SPEED);
-    rightMotor->setVelocity(-MAX_SPEED);
+    leftMotor->setVelocity(MAX_TURN_SPEED);
+    rightMotor->setVelocity(-MAX_TURN_SPEED);
 }
 
 static void ResetMotors() {
@@ -48,8 +49,20 @@ static void ResetMotors() {
     cameraMotor->setVelocity(0);
 }
 
-static bool IsObject(){
-return true;
+int* GetRGB(int width, int height, int horizOffset, int vertOffset) {
+    int* array = new int[3];
+    array[0] = camera->imageGetRed(camera->getImage(), width, horizOffset, height-1-vertOffset);
+    array[1] = camera->imageGetGreen(camera->getImage(), width, horizOffset, height-1-vertOffset);
+    array[2] = camera->imageGetBlue(camera->getImage(), width, horizOffset, height-1-vertOffset);
+    return array;
+}
+
+static bool IsObject(int* rgb){
+    if (rgb[0] < 100 || rgb[2] < 100) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 static void ThereAndBackAgain(Robot* robot){
@@ -82,26 +95,29 @@ int main(int argc, char** argv) {
     cameraMotor = robot->getMotor("camera motor");
     cameraMotor->setPosition(INFINITY);
     cameraMotor->setVelocity(0.0);
-
+    ResetMotors();
 
     //Local variables
     //int examlpeNum = 0;
     int currAngle = 0;
+    int width = camera->getWidth();
+    int height = camera->getWidth();
 
     //*********Robot AI goes here*********
     
-    if(IsObject()){
-      ThereAndBackAgain(robot);
-    }
-    while (currAngle < 375){
-      TurnLeft();
-      robot->step(TIME_STEP*3);
-      ResetMotors();
-      if(IsObject()){
-      ThereAndBackAgain(robot);
-      }
-      
-        currAngle = currAngle + 15;
+    
+    while (robot->step(TIME_STEP) != -1) {
+        //Gets the RGB of the bottom middle pixel on the camera
+        int* rgb = GetRGB(width, height, width/2 , 18);
+        //Print any test messages here
+        std::cout << "Red: " << rgb[0] << ", Green: " << rgb[1] << ", Blue: " << rgb[2] << std::endl;
+        std::cout << IsObject(rgb) << std::endl;
+        if (IsObject(rgb)) {
+            ThereAndBackAgain(robot);
+        } else {
+            TurnLeft();
+        }
+        delete[] rgb;
     }
     
     //End of AI
